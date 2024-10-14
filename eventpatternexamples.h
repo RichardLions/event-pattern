8967a68abdd30ba08b9e3b3deb7d38977fd65a7c
Richard Allen <Richard.Allen@ubisoft.com>
2024-10-14 13:50:19 +0100

    Blank project setup

---
+++ eventpatternexamples.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
+
+TEST_CASE("Event Pattern - Unit Tests")
+{
+}
+
+TEST_CASE("Event Pattern  - Benchmarks")
+{
+    BENCHMARK("Benchmark")
+    {
+    };
+}
